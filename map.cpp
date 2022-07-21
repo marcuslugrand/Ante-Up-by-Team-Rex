c0a5c5b68fa44a6d021167909221854a68f8e9a6
marcuslugrand <80719289+marcuslugrand@users.noreply.github.com>
2022-07-21 17:12:42 -0400

    Add files via upload

---
+++ map.cpp
@@ -0,0 +1,28 @@
+#include "map.h"
+#include <map>
+#include <iterator>
+
+
+const int map::size()
+{
+    return size;
+}
+
+void map::insert(std::vector <Hand> hand)
+{
+    mapHand.insert(std::pair<hand.cards, hand.quality>);
+    ++size;
+}
+
+const int map::find(std::vector <Hand> hand)
+{
+    for (auto iter = mapHand.find(hand); iter != mapHand.end(); iter++) {
+        return iter -> first;
+    }
+}
+std::vector<Hand> map::find(const int &quality)
+{
+    for (auto iter = mapHand.find(&quality); iter != mapHand.end(&quality); iter++) {
+        hand.push_back();
+    }
+}
