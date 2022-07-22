1fb3ea9c7357f1b2144c1d9cf9056bb3ad6d846d
Gage-Zahn <92762355+Gage-Zahn@users.noreply.github.com>
2022-07-21 17:11:54 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -58,13 +58,13 @@ int main() {
     //importFile("poker-hand-training-true.data");
     //importFile("poker-hand-testing.data");
     
-    sf::RenderWindow window(sf::VideoMode(1600, 800), "Ante Up");
+    sf::RenderWindow window(sf::VideoMode(1800, 800), "Ante Up");
     
-    //----Shapes---
+    //Divider
     sf::RectangleShape divider(sf::Vector2f(10, 800));
     divider.setPosition(800, 0);
 
-        //Table
+    //Table
     int offset = 380;
     sf::RectangleShape bottom(sf::Vector2f(420, 700));
     bottom.setPosition(offset,100);
@@ -81,7 +81,8 @@ int main() {
     middle.setFillColor(sf::Color::Transparent);
     middle.setOutlineColor(sf::Color::White);
     middle.setOutlineThickness(5);
-            //Titles
+    
+    //Titles
     sf::Font font;
     if (!font.loadFromFile("arial.ttf"))
     {
@@ -96,6 +97,14 @@ int main() {
     subtitle2.setPosition(530, 60);
     sf::Text subtitle3("Run Time", font);
     subtitle3.setPosition(670, 60);
+    sf::Text option("Options", font);
+    option.setPosition(120, 1);
+    sf::Text option1("How good is this hand?", font);
+    option1.setPosition(40, 47);
+    option1.setCharacterSize(20);
+    sf::Text option2("What hands are this good?", font);
+    option2.setPosition(40, 97);
+    option2.setCharacterSize(20);
 
     //Cards
     sf::Texture allCards;
@@ -116,18 +125,84 @@ int main() {
             cardImages[make_pair(suitReal, rank)] = temp;
         }
     }
+    
+    //Selection
+    sf::RectangleShape select(sf::Vector2f(73, 98));
+    select.setFillColor(sf::Color::Transparent);
+    select.setOutlineColor(sf::Color::Green);
+    select.setOutlineThickness(2);
 
+    //Options
+    sf::RectangleShape button1(sf::Vector2f(20, 20));
+    button1.setFillColor(sf::Color::Transparent);
+    button1.setOutlineColor(sf::Color::White);
+    button1.setOutlineThickness(1);
+    button1.setPosition(10, 50);
+    sf::RectangleShape button2(sf::Vector2f(20, 20));
+    button2.setFillColor(sf::Color::Transparent);
+    button2.setOutlineColor(sf::Color::White);
+    button2.setOutlineThickness(1);
+    button2.setPosition(10, 100);
+    sf::RectangleShape button3(sf::Vector2f(20, 20));
+    button3.setFillColor(sf::Color::Transparent);
+    button3.setOutlineColor(sf::Color::White);
+    button3.setOutlineThickness(1);
+    button3.setPosition(10, 150);
+    sf::RectangleShape button4(sf::Vector2f(20, 20));
+    button4.setFillColor(sf::Color::Transparent);
+    button4.setOutlineColor(sf::Color::White);
+    button4.setOutlineThickness(1);
+    button4.setPosition(10, 200);
 
+    bool displayCards = false;
     while (window.isOpen())
     {
+       
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (event.type == sf::Event::MouseButtonReleased) {
+
+                if (event.mouseButton.button == sf::Mouse::Left){
+                    sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+                    //option 1
+                    if (mousePosition.y < 70 && mousePosition.y > 50 && mousePosition.x < 30 && mousePosition.x > 10) {
+                        button1.setFillColor(sf::Color::White);
+                        button2.setFillColor(sf::Color::Transparent);
+                        button3.setFillColor(sf::Color::Transparent);
+                        button4.setFillColor(sf::Color::Transparent);
+                        displayCards = true;
+                    }
+                    //option 2
+                     if (mousePosition.y < 120 && mousePosition.y > 100 && mousePosition.x < 30 && mousePosition.x > 10) {
+                        button1.setFillColor(sf::Color::Transparent);
+                        button2.setFillColor(sf::Color::White);
+                        button3.setFillColor(sf::Color::Transparent);
+                        button4.setFillColor(sf::Color::Transparent);
+                    }
+                     /*//option 3
+                    if (mousePosition.y < game.GetHeight() * 32 && game.state == 1) {
+
+                    }
+                    //option 4
+                    if (mousePosition.y < game.GetHeight() * 32 && game.state == 1) {
+
+                    }
+                    //cards
+                    if (mousePosition.y < game.GetHeight() * 32 && game.state == 1) {
+
+                    }
+                    */
+                }
+            }
         }
 
         window.clear();
+        
+        //draw static elements
         window.draw(divider);
         window.draw(bottom);
         window.draw(top);
@@ -136,9 +211,24 @@ int main() {
         window.draw(subtitle1);
         window.draw(subtitle2);
         window.draw(subtitle3);
+        window.draw(button1);
+        window.draw(button2);
+        window.draw(button3);
+        window.draw(button4);
+        window.draw(option1);
+        window.draw(option2);
+        window.draw(option);
+
+        //draw dynamic elements
+        if(displayCards){
+            for (int suit = 1; suit <= 4; suit++) {
+                for (int rank = 1; rank <= 13; rank++) {
+                    cardImages[make_pair(suit, rank)].setPosition(820 + 75 * (rank - 1), 100 * suit);
+                    window.draw(cardImages[make_pair(suit, rank)]);
+                }
+            }
+        }
         
-        cardImages[make_pair(2, 1)].setPosition(900, 400);
-        window.draw(cardImages[make_pair(2, 1)]);
         window.display();
     }
 
