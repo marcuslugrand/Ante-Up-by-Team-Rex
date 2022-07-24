3a53e3cf93373e0a1a1c63b720b003ed7075d043
weiss-ben <weissb@ufl.edu>
2022-07-24 22:40:17 +0300

    Added unit-test files

---
+++ test-unit/test_unordered_map.cpp
@@ -0,0 +1,17 @@
+#include "../Ante-Up-by-Team-Rex/unordered_map.cpp"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+/*
+	To check output (At the find_the digits directory):
+		g++ -std=c++14 -Werror -Wuninitialized -o test test-unit/test.cpp && ./test
+*/
+/*
+ * Insertion
+ */
+/*
+ * Search
+ */
+/*
+ * Access
+ */
