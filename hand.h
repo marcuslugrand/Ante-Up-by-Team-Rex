3013250c9a2c659d3e094f7d08779bc09443f1df
weiss-ben <weissb@ufl.edu>
2022-07-24 20:55:23 +0300

    Added a comparison function for the card and hand classes

--- hand.h
+++ hand.h
@@ -4,13 +4,14 @@ struct Card {
 	int suit;
 	int rank;
 	Card(int suit, int rank);
-    bool cardCompare(Card& a, Card& b);
+    bool cardCompare(Card& a);
 };
 
 struct Hand {
 	std::vector<Card> cards;
 	int qualty;
 	Hand(std::vector<Card> &cards, int qualty);
+    bool handCompare(Hand& a);
 };
 
 Hand::Hand(std::vector<Card> &cards, int qualty) {
@@ -23,9 +24,19 @@ Card::Card(int suit, int rank) {
 	this->rank = rank;
 }
 
-bool Card::cardCompare(Card& a, Card& b)
+bool Card::cardCompare(Card& a)
 {
-    if(a.suit != b.suit || a.rank != b.rank)
+    if(this->suit != a.suit || this->rank != a.rank)
         return false;
+    return true;
+}
+
+bool Hand::handCompare(Hand& a)
+{
+    for(int i = 0; i < 5; ++i)
+    {
+        if(!this->cards.at(i).cardCompare(a.cards.at(i)))
+            return false;
+    }
     return true;
 }
