964c28061ed1ad135ed1ef24bef6da6839febbcb
marcuslugrand <80719289+marcuslugrand@users.noreply.github.com>
2022-07-30 23:59:19 -0400

    Update hand_map.h

--- src/hand_map.h
+++ src/hand_map.h
@@ -1,7 +1,9 @@
 #include <vector>
 #include <iostream>
+#include <string>
 #include "hand.h"
 
+//Node class for tree
 class Node {
 public:
     std::vector<Card> first;
@@ -13,6 +15,9 @@ public:
     Node* right;
 
     Node() {
+        int second = 0;
+        int third = 0;
+        int balance = 0;
         left = nullptr;
         right = nullptr;
     };
@@ -27,76 +32,39 @@ public:
     };
 };
 
+//Map class for main
 class hand_map {
-
 public:
-    //static class map* root;
     Node* root;
     int nodeCount;
     int depth{};
-    int index = 0;
-    hand_map();
-    int getHeight(Node* root);
-    Node* intial() { return root; };
-    void setInitial(Node* root) {
-        this->root = root;
-    };
 
     std::vector<Node*> *nodes;
-    //std::vector<Node> nodes = new std::vector<Node>;
+    
+    hand_map();
 
-    void insert(Hand hand);
+    int getHeight(Node* root);
+    Node* intial();
+    void setInitial(Node* root);
 
     int getnodeCount();
 
     int recHand(int sum, Node* root);
-
-    Node* insertMap(Node* root, std::vector<Card> first, int value, std::vector<Node*>* nodes);
-    void findQuality(int second, std::vector<Hand>& qualtoHan2, std::vector<Node*> *nodess);
-    int findHand(Node* root, std::vector<Card> first, std::vector<Node*>* nodes);
+    void searchAllHands(Node* root, int second ,std::vector<Hand> &qualtoHand);
+    
+    Node* insertMap(Node* root, std::vector<Card> first, int value);
+    void findQuality(int second, std::vector<Hand>& qualtoHan2);
+    int findHand(Node* root, std::vector<Card> first);
 
     int balanceFactor(Node* root);
 
     Node* rotateRight(Node* node);
-
-    //Rotates node left
     Node* rotateLeft(Node* node);
-
-    //Rotates node left right
     Node* rotateLeftRight(Node* node);
-
-    //Rotates node right left
     Node* rotateRightLeft(Node* node);
-
-    void preOrder(Node* root, int second ,std::vector<Hand> &qualtoHand);
-
     
-    //store hands together by quality
-    std::vector<std::vector<Hand>> qualtoHand;
-    
-    // Iterate through tree in order
-    /*void iterforQual(map* head, int quality)
-    {
-        if (root == nullptr) {
-        return;
-        }
-        if (head->second == quality) {
-            qualtoHand.push_back(first);
-        }
-        if (head->left != nullptr) {
-            iterforQual(head->left);
-        }
-        if (head->right != nullptr) {
-            iterforQual(head->right);
-        }
-    }*/
-
-    // Returns number of elements in the map
-    int size(void);
-
-    // Insert key and value
+    void insert(Hand hand);
     const int find(const Hand& hand);
-
     std::vector<Hand> find(const int quality);
 };
 
